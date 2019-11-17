e88f39f792670880e40791e2dcbbcb03f7ebaad4
Zachary Tipnis <tipniz@rpi.edu>
2019-11-17 14:54:48 -0500

    eslint error fixed

--- list.js
+++ list.js
@@ -1,48 +1,52 @@
 module.exports = function (address){
-	this.address = address;
-	this.validate = async (client) => {
-		if(client){
-			try{
-				let result = await client.query("SELECT * FROM \"public\".\"lists\" WHERE address = $1", [address])
-				return result.rowCount > 0;
-			}catch(err){
-				throw new Error("Lookup for list "+ address + " failed. " + err)
-			}
-            
-        }else{
+    this.address = address;
+    this.validate = async (client) => {
+        if (client){
+            try {
+                const result = await client.query("SELECT * FROM \"public\".\"lists\" WHERE address = $1", [address])
+                return result.rowCount > 0;
+            }
+            catch (err){
+                throw new Error("Lookup for list "+ address + " failed. " + err)
+            }
+
+        }
+        else {
             throw new Error("Client does not exist")
         }
-	}
-	this.recipients = async (client) => {
-		if(client){
-			try{
-				let result = await client.query("SELECT A.email FROM \"public\".\"lists\" AS l RIGHT JOIN \"public\".\"listUsers\" AS A ON A.lid=l.id WHERE l.address=$1", [address])
-				return result.rows.map((rw) => rw.email);
-			}catch(err){
-				throw new Error("Lookup recipients for list "+ address + " failed. " + err)
-			}
-            
-        }else{
+    }
+    this.recipients = async (client) => {
+        if (client){
+            try {
+                const result = await client.query("SELECT A.email FROM \"public\".\"lists\" AS l RIGHT JOIN \"public\".\"listUsers\" AS A ON A.lid=l.id WHERE l.address=$1", [address])
+                return result.rows.map((rw) => rw.email);
+            }
+            catch (err){
+                throw new Error("Lookup recipients for list "+ address + " failed. " + err)
+            }
+
+        }
+        else {
             throw new Error("Client does not exist")
         }
     }
 
-    this.verp = function(command){
-    	switch(command){
-    		case "sub":
-    			return address.replace('@', '%sub%@')
-    		case "unsub":
-    			return address.replace('@', '%unsub%@')
-    		default:
-    			return "none";
-    	}
+    this.verp = function (command){
+        switch (command){
+            case "sub":
+                return address.replace('@', '%sub%@')
+            case "unsub":
+                return address.replace('@', '%unsub%@')
+            default:
+                return "none";
+        }
     }
-    this.unverp = function(){
-    	let regex = /%.*%/
-    	return {
-    		command: (address.match(/%(.*?)%/) || []).pop(),
-    		email: address.replace(regex, "")
-    	}
+    this.unverp = function (){
+        const regex = /%.*%/
+        return {
+            command: (address.match(/%(.*?)%/) || []).pop(),
+            email: address.replace(regex, "")
+        }
     }
 
 
